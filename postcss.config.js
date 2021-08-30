f37f3e121c45bc1b0c48ee372148a44763f07e2e
Nicholas Argiropoulo <nik@knak.com>
2021-08-30 07:15:01 -0700

    Another effort at solving prod issue based on https://github.com/vercel/next.js/discussions/18709

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,3 @@
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    content: ["./components/**/*.js", "./pages/**/*.js"],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-];
-
 module.exports = {
   plugins: [
     "postcss-import",
