91ebc31e4ef69abd8db93a0929f049c85e1d2ea2
Nicholas Argiropoulo <nik@knak.com>
2021-08-31 06:47:00 -0700

    Updating purge contents to resolve prod issue.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  purge: [],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./_layouts/**/*.{js,ts,jsx,tsx}",
+    "./_includes/**/*.{js,ts,jsx,tsx}",
+    "./helpers/**/*.{js,ts,jsx,tsx}",
+    "./api/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
